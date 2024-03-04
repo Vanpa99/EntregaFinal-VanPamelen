55abf6d9cbbfacbe4e7d0a4faebd41d5225e4f7f
Vanpa <mativanpa99@gmail.com>
2024-03-04 00:28:08 -0300

    Mejora de preentrega1

--- Js/script.js
+++ Js/script.js
@@ -1,39 +1,37 @@
+let facturasDocena = 5000;
+let bizcochosKg = 3000;
+let masasFinasKg = 20000;
+let alfajoresUnidad = 1000;
+let precioFinal = 0;
 
-// Productos con precio
-const listaProd = {
-    facturasDocena: 5000,
-    bizcochosKg: 3000,
-    masasFinasKg: 20000,
-    alfajoresUnidad: 1000
-};
 
-let valorTotal = 0;
-do {
-    let elegir = parseInt(prompt("¡Bienvenido! ¿Que desea comprar?: \n1. Facturas x12    \n2. Bizcochos x Kg.    \n3. Masas Finas x Kg.  \n4. Alfajores Artesanales x Unidad.    \n5. Chipa x Unidad."));
+let continuarComprando = true;
 
-    let producto;
-    switch (elegir) {
-        case 1:
-            producto = "facturasDocena";
-            break;
-        case 2:
-            producto = "bizcochosKg";
-            break;
-        case 3:
-            producto = "masasFinasKg";
-            break;
-        case 4:
-            producto = "alfajoresUnidad";
-            break;
+while (continuarComprando) {
+    const productos = prompt("¡Bienvenido a Bakery! \n\nIngrese el número del producto que desea comprar:\n1. Facturas (Docena). \n2. Bizcochos (Kilo). \n3. Masas Finas (Kilo).  \n4. Alfajor Artesanal (Unidad). \n\n Para finalizar la compra ingrese 0:");
+
+    switch (productos) {
+        case "1":
+            precioFinal += facturasDocena;
+            alert("Se agregó Facturas (Docena). Precio: $" + facturasDocena + ". Precio total: $" + precioFinal );
+        break;
+        case "2":
+          precioFinal += bizcochosKg;
+          alert("Se agregó Bizcochos (Kilo). Precio: $" + bizcochosKg + ". Precio total: $" + precioFinal );            
+        break;
+        case "3":
+          precioFinal += masasFinasKg;
+          alert("Se agregó Masas Finas (Kilo). Precio: $" + masasFinasKg + ". Precio total: $" + precioFinal );            
+        break;
+        case "4":
+          precioFinal += alfajoresUnidad;
+          alert("Se agregó Alfajor Artesanal (Unidad). Precio: $" + alfajoresUnidad + ". Precio total: $" + precioFinal );
+        break;
+        case "0":
+            continuarComprando = false;
+            alert("El total de su compra es de $" + precioFinal + ". Muchas gracias ¡Que la disfrute!");
+        break;
         default:
-            alert("Producto no encontrado u opción inválida. Por favor, vuelva a intentarlo.");
+            alert("Producto no encontrado. Elija una opcion valida o escriba '0' para finalizar la compra.");
     }
-
-    if (producto) {
-        valorTotal += listaProd[producto];
-        alert("Ha seleccionado " + producto + ". Precio: $" + listaProd[producto]);
-      }
-
-      var continuar = parseInt(prompt("¿Desea comprar algo mas? \n1. Si \n2. No"));
-
-} while (continuar === "si");
+}
