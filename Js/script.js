6cdd84d8aaa37be7870e15c20e73d3d8e954801e
Vanpa <mativanpa99@gmail.com>
2024-03-08 14:11:57 -0300

    se agrego fuction

--- Js/script.js
+++ Js/script.js
@@ -5,33 +5,39 @@ let alfajoresUnidad = 1000;
 let precioFinal = 0;
 
 
-let continuarComprando = true;
+function usuario() {
+  let nombreUsuario = prompt("Por favor, ingresa tu nombre:");
 
-while (continuarComprando) {
-    const productos = prompt("¡Bienvenido a Bakery! \n\nIngrese el número del producto que desea comprar:\n1. Facturas (Docena). \n2. Bizcochos (Kilo). \n3. Masas Finas (Kilo).  \n4. Alfajor Artesanal (Unidad). \n\n Para finalizar la compra ingrese 0:");
 
-    switch (productos) {
-        case "1":
-            precioFinal += facturasDocena;
-            alert("Se agregó Facturas (Docena). Precio: $" + facturasDocena + ". Precio total: $" + precioFinal );
-        break;
-        case "2":
-          precioFinal += bizcochosKg;
-          alert("Se agregó Bizcochos (Kilo). Precio: $" + bizcochosKg + ". Precio total: $" + precioFinal );            
-        break;
-        case "3":
-          precioFinal += masasFinasKg;
-          alert("Se agregó Masas Finas (Kilo). Precio: $" + masasFinasKg + ". Precio total: $" + precioFinal );            
-        break;
-        case "4":
-          precioFinal += alfajoresUnidad;
-          alert("Se agregó Alfajor Artesanal (Unidad). Precio: $" + alfajoresUnidad + ". Precio total: $" + precioFinal );
-        break;
-        case "0":
-            continuarComprando = false;
-            alert("El total de su compra es de $" + precioFinal + ". Muchas gracias ¡Que la disfrute!");
-        break;
-        default:
-            alert("Producto no encontrado. Elija una opcion valida o escriba '0' para finalizar la compra.");
-    }
+  let continuarComprando = true;
+
+  while (continuarComprando) {
+      const productos = prompt("¡Bienvenido a Bakery, " + nombreUsuario + "! \n\nIngrese el número del producto que desea comprar:\n1. Facturas (Docena). \n2. Bizcochos (Kilo). \n3. Masas Finas (Kilo).  \n4. Alfajor Artesanal (Unidad). \n\n Para finalizar la compra ingrese 0:");
+
+      switch (productos) {
+          case "1":
+              precioFinal += facturasDocena;
+              alert("Se agregó Facturas (Docena). Precio: $" + facturasDocena + ". Precio total: $" + precioFinal );
+          break;
+          case "2":
+            precioFinal += bizcochosKg;
+            alert("Se agregó Bizcochos (Kilo). Precio: $" + bizcochosKg + ". Precio total: $" + precioFinal );            
+          break;
+          case "3":
+            precioFinal += masasFinasKg;
+            alert("Se agregó Masas Finas (Kilo). Precio: $" + masasFinasKg + ". Precio total: $" + precioFinal );            
+          break;
+          case "4":
+            precioFinal += alfajoresUnidad;
+            alert("Se agregó Alfajor Artesanal (Unidad). Precio: $" + alfajoresUnidad + ". Precio total: $" + precioFinal );
+          break;
+          case "0":
+              continuarComprando = false;
+              alert("El total de su compra es de $" + precioFinal + ". Muchas gracias, " + nombreUsuario + " ¡Que la disfrute!");
+          break;
+          default:
+              alert(nombreUsuario + ", su producto no ha sido encontrado. Elija una opcion valida o escriba '0' para finalizar la compra.");
+      }
+  }
 }
+usuario();
