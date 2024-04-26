b795babdaf01bf706993a98a80326d254e80c859
Vanpa <mativanpa99@gmail.com>
2024-04-26 20:56:19 -0300

    Corregir

--- Js/productos.js
+++ Js/productos.js
@@ -1,27 +1,27 @@
-const listaProductos =
-[
-    { 
-        id: 1,
-        nombre: "Facturas (Docena)",
-        img: "./Img/facturas.jpg",
-        precio: 5000 
-    },
-    { 
-        id: 2,
-        nombre: "Bizcochos (Kilo)", 
-        img: "./Img/bizcochos.jpg", 
-        precio: 3000 
-    },
-    { 
-        id: 3, 
-        nombre: "Masas Finas (Kilo)",  
-        img: "./Img/masas-finas.jpg,",
-        precio: 200 
-    },
-    { 
-        id: 4, 
-        nombre: "Alfajores (Unidad)",  
-        img: "./Img/alf-art.jpg", 
-        precio: 1000 
-    }
-]
+// const listaProductos =
+// [
+//     { 
+//         id: 1,
+//         nombre: "Facturas (Docena)",
+//         img: "./Img/facturas.jpg",
+//         precio: 5000 
+//     },
+//     { 
+//         id: 2,
+//         nombre: "Bizcochos (Kilo)", 
+//         img: "./Img/bizcochos.jpg", 
+//         precio: 3000 
+//     },
+//     { 
+//         id: 3, 
+//         nombre: "Masas Finas (Kilo)",  
+//         img: "./Img/masas-finas.jpg",
+//         precio: 200 
+//     },
+//     { 
+//         id: 4, 
+//         nombre: "Alfajores (Unidad)",  
+//         img: "./Img/alf-art.jpg", 
+//         precio: 1000 
+//     }
+// ]
